27c74f15d662fda0984611a1a28e386813a9a7bf
trahulprabhu38 <trahulprabhu38@gmail.com>
2025-04-15 12:26:42 +0530

    doner

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-// Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve static files from the root directory instead of "public"
+app.use(express.static(__dirname));
+
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
